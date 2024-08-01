773557817b432b43f41045e97263eee3c21a6a4c
Youlin Huang <tvgryphon2@gmail.com>
2024-08-01 16:20:32 -0700

    documentation + backspace and nextlining

--- kernel.c
+++ kernel.c
@@ -11,15 +11,75 @@
 #define ENTER_KEYCODE 0x1C
 #define BACKSPACE_KEYCODE 0x08
 
+#define false 0
+#define true 1
+
+#define for_each(item, array) \
+            for(int keep = 1, \
+                    count = 0, \
+                    size = sizeof(array) / sizeof *(array); \
+                keep && count != size; \
+                keep = !keep, count++) \
+                for(item = (array) + count; keep; keep = !keep)
+
 extern unsigned char keyboard_map[128];
 extern void keyboard_handler(void);
 extern void load_idt(unsigned long *idt_ptr);
 extern char read_port(unsigned short port);
-extern void write_port(unsigned short port, unsigned char data);
+extern void write_port(unsigned short port, unsigned char data);//This would be the equivalent of Linux' outb
 
 unsigned int current_loc = 0;
+unsigned int currentLine= 0;
 
 char *vidptr = (char*)0xb8000;
+
+struct line{
+    char contents[COLUMNS_IN_LINE];
+    int index;
+};
+
+
+
+//STRUCT LINE
+
+struct line line_initialize(){
+    struct line l = {
+        {0},
+        -1
+    };
+    return l;
+}
+
+int line_isAtEnd(struct line line){
+    return line.index == COLUMNS_IN_LINE - 1;
+}
+
+int line_write(struct line *line, char c){
+    if(line_isAtEnd(*line) == false) {
+        line->contents[++line->index] = c;
+        return true;
+    }
+    return false;
+}
+
+int line_backspace(struct line *line){
+    if(line->index >= 0){
+        line->contents[line->index--] = 0;
+        return true;
+    }
+    return false;
+}
+
+int line_isEmpty(struct line line){
+    for_each(char* c, line.contents){
+        if(c != 0){
+            return false;
+        }
+    }
+    return true;
+}
+
+struct line lines[25];
 
 struct IDT_entry{
 
@@ -37,7 +97,7 @@ void idt_init(void){
     unsigned long keyboard_address;
     unsigned long idt_address;
     unsigned long idt_ptr[2];
-
+    
     keyboard_address = (unsigned long) keyboard_handler;
     IDT[0x21].offset_lowerbits = keyboard_address & 0xffff;
     IDT[0x21].selector = 0x08;
@@ -82,14 +142,20 @@ void kprint(const char *str){
 }
 
 void kprint_newline(void){
+    currentLine++;
     unsigned int line_size = BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE;
-    current_loc = current_loc + (line_size - current_loc % (line_size));
+    current_loc = currentLine * line_size;
+    lines[currentLine] = line_initialize();
 }
 
 void kprint_backspace(void){
     vidptr[current_loc - 2] = ' ';
     vidptr[current_loc - 1] = 0x07;
     current_loc -= 2;
+    if(!line_backspace(&lines[currentLine])){
+        currentLine--;
+        current_loc = currentLine * BYTES_FOR_EACH_ELEMENT * COLUMNS_IN_LINE + (lines[currentLine].index + 1) * BYTES_FOR_EACH_ELEMENT;
+    }
 }
 
 void clear_screen(void){
@@ -98,6 +164,15 @@ void clear_screen(void){
         vidptr[i++] = ' ';
         vidptr[i++] = 0x07;
     }
+}
+
+void update_cursor(int x, int y){
+    unsigned short int pos = y * COLUMNS_IN_LINE + x;
+
+    write_port(0x3d4, 0x0f);
+    write_port(0x3d5, (pos & 0xff));
+    write_port(0x3d4, 0x0e);
+    write_port(0x3d5, ((pos >> 8) & 0xff));
 }
 
 void keyboard_handler_main(void){
@@ -109,18 +184,24 @@ void keyboard_handler_main(void){
 
     if(status & 0x01){
         keycode = read_port(KEYBOARD_DATA_PORT);
+        unsigned char key = keyboard_map[(unsigned char) keycode];
         if(keycode < 0)
             return;
-        if(keycode == ENTER_KEYCODE){
+        if(key == *"\n"){
             kprint_newline();
-            return;
-        }
-        if(keycode == BACKSPACE_KEYCODE){
+        }else if(key == *"\b"){
             kprint_backspace();
-            return;
+        }else if(line_write(&lines[currentLine], key)){
+            vidptr[current_loc++] = key;
+            vidptr[current_loc++] = 0x07;
+        }else{
+            kprint_newline();
+            if(line_write(&lines[currentLine], key)){
+                vidptr[current_loc++] = key;
+                vidptr[current_loc++] = 0x07;
+            }
         }
-        vidptr[current_loc++] = keyboard_map[(unsigned char) keycode];
-        vidptr[current_loc++] = 0x07;
+        update_cursor(lines[currentLine].index, currentLine);
     }
 }
 
@@ -129,6 +210,7 @@ void kmain(void);
 void kmain(void){
     const char *str = "blacktar";
     clear_screen();
+    lines[currentLine] = line_initialize();
 	kprint(str);
     kprint_newline();
     kprint_newline();
